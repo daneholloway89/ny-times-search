13f94a2ef365fbcc5fa2c2072b419bc05bf37141
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-04-27 13:58:16 -0400

    display results

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,24 +1,25 @@
 
 var submitButton = $("#submit");
+var resultDiv = $("#resultsbox");
 
 var topic = "";
 var numberOfArticles = 1;
 var startYear = 1900;
+var startDate = "";
 var endYear = 9999;
+var endDate = "";
 
 var apiKey = "7yTLyZQwpepHBSYCnAmwybWzl3NJ53f0";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&api-key=" + apiKey;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&api-key=" + apiKey; //+ "&begin_date=" + startDate +"&end_date=" + endDate
 // Things to accomplish:
 submitButton.on("click", function () {
     event.preventDefault(); 
     topic = $("#search-term").val().trim();
     numberOfArticles = $("#records").val();
     startYear = $("#start-year").val();
+    startDate = startYear.toString() + "0101"
     endYear = $("#end-year").val();
-    console.log(topic);
-    console.log(numberOfArticles);
-    console.log(startYear);
-    console.log(endYear);
+    endDate = endYear.toString() + "0101"
     callAPI();
 })
 
@@ -30,9 +31,8 @@ function callAPI() {
         var results = payload.response.docs;
     
         for (var i = 0; i < results.length; i++) {
-            console.log(results[i].web_url);
-            console.log(results[i].snippet);
-            console.log(results[i].lead_paragraph);
+            var newDiv = $("<div>", { text: "URL" + results[i].web_url + "\nSnippet: " + results[i].snippet + "\nLead Paragraph: " + results[i].lead_paragraph})
+            resultDiv.append(newDiv);
         }
     });
 }
