b372bcf772eb2d58d4cb080d6b99ec0661ec30d6
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-04-27 13:20:35 -0400

    successful call to API

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,11 +1,26 @@
 
+var submitButton = $("#submit");
+
 var topic = "Trump";
-var apiKey="7yTLyZQwpepHBSYCnAmwybWzl3NJ53f0";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic +"&api-key=" + apiKey;
+var apiKey = "7yTLyZQwpepHBSYCnAmwybWzl3NJ53f0";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&api-key=" + apiKey;
 // Things to accomplish:
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
+$("#submit").on("click", function () {
+    event.preventDefault(); 
+    callAPI();
+})
+
+function callAPI() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (payload) {
+        var results = payload.response.docs;
+    
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i].web_url);
+            console.log(results[i].snippet);
+            console.log(results[i].lead_paragraph);
+        }
+    });
+}
