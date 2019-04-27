d793e196a6f7b66745423c4b0ed28fd5782ddcaa
spbryan <seanbryan@comcast.com>
2019-04-27 13:40:18 -0400

    read from form

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -2,13 +2,23 @@
 var submitButton = $("#submit");
 
 var topic = "";
+var numberOfArticles = 1;
+var startYear = 1900;
+var endYear = 9999;
+
 var apiKey = "7yTLyZQwpepHBSYCnAmwybWzl3NJ53f0";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&api-key=" + apiKey;
 // Things to accomplish:
 submitButton.on("click", function () {
     event.preventDefault(); 
     topic = $("#search-term").val().trim();
+    numberOfArticles = $("#records").val();
+    startYear = $("#start-year").val();
+    endYear = $("#end-year").val();
     console.log(topic);
+    console.log(numberOfArticles);
+    console.log(startYear);
+    console.log(endYear);
     callAPI();
 })
 
